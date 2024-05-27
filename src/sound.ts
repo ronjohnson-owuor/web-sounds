918d8d9640094f72d53addb6caf9230eb5eccb39
ronjohnson-owuor <ronjohnsonowuor83@gmail.com>
2024-05-27 22:05:37 +0300

    added more sounds and rewrote the code

--- src/sound.ts
+++ src/sound.ts
@@ -1,9 +1,12 @@
 const sounds  = {
-    "clap":{
-        "audience1":"https://opengameart.org/sites/default/files/audio_preview/applause.wav.mp3",
-        "audience_long_deep" : "https://opengameart.org/sites/default/files/audio_preview/applause-clapping-church-crowd-immersive-preview.ogg.mp3",
-        "audience_firework":"https://opengameart.org/sites/default/files/applause_fireworks.mp3"
-    }
+        audience1:"https://opengameart.org/sites/default/files/audio_preview/applause.wav.mp3",
+        audience_long_deep : "https://opengameart.org/sites/default/files/audio_preview/applause-clapping-church-crowd-immersive-preview.ogg.mp3",
+        audience_firework:"https://opengameart.org/sites/default/files/applause_fireworks.mp3",
+        car_passing:"https://opengameart.org/sites/default/files/audio_preview/car1.wav.mp3",
+        car_hoot:"https://opengameart.org/sites/default/files/audio_preview/car2.wav.mp3",
+        car_engine_rave:"https://opengameart.org/sites/default/files/audio_preview/car_acceleration.ogg.mp3",
+        car_fixing:"https://opengameart.org/sites/default/files/scifiprev.mp3",
+        default:"https://opengameart.org/sites/default/files/audio_preview/applause.wav.mp3"
 }
 
 export default sounds;
--- src/sounds.ts
+++ src/sounds.ts
@@ -4,7 +4,7 @@ import sounds from "./sound";
 
 function playAudio(url:string,time:number){
     // highest duration a video can play is 5seconds
-    let duration = 5 * 1000;
+    let duration = 7 * 1000;
     let audio = new Audio();
     audio.src=url;
     audio.onloadedmetadata = () =>{
@@ -23,16 +23,8 @@ function playAudio(url:string,time:number){
 }
 
 
-export function applause (time?:number){
-    // time to be given in seconds
-    playAudio(sounds.clap.audience1,time?time:5);
-}
-
-
-export function applause_deep (time?:number){
-    playAudio(sounds.clap.audience_long_deep,time?time:5);
-}
-
-export function applause_firework (time?:number){
-    playAudio(sounds.clap.audience_firework,time?time:5);
+export function play_reaction(sound_name:string,time?:number){
+        // time to be given in seconds
+        const soundUrl = sounds[sound_name as keyof typeof sounds] || sounds.default;
+        playAudio(soundUrl,time?time:7);
 }
