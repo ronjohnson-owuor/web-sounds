d131173d1edaf42ccc3eb32fdcf9b7639d1de0bf
ronjohnson-owuor <ronjohnsonowuor83@gmail.com>
2024-05-31 20:30:22 +0300

    added play custom function to allow users to load their own audio

--- src/sounds.ts
+++ src/sounds.ts
@@ -4,12 +4,18 @@ import sounds from "./sound";
 
 function playAudio(url:string,time:number){
     // highest duration a video can play is 5seconds
-    let duration = 7 * 1000;
+    let duration = time * 1000;
     let audio = new Audio();
     audio.src=url;
     audio.onloadedmetadata = () =>{
         // determine which duration is shorter the audios duration or the 5seconds
-        duration = Math.min(audio.duration,5) * 1000;
+        if(time == -1){
+            // play the whole video without posing
+            duration = (audio.duration) * 1000;
+        }else{
+            duration = Math.min(audio.duration,time) * 1000;  
+        }
+        
     }
     audio.play();
     // pause after the time is reached or the video is over
@@ -25,6 +31,12 @@ function playAudio(url:string,time:number){
 
     export function play_reaction(sound_name:string,time?:number){
         // time to be given in seconds
-        const soundUrl = sounds[sound_name as keyof typeof sounds] || sounds.default;
+        //play the whole audio if time = -1;
+        let  soundUrl = sounds[sound_name as keyof typeof sounds] || sounds.default;
         playAudio(soundUrl,time?time:7);
-}
+    }
+
+    export function play_custom(custom_url:string,time?:number){
+        // user can add his own custom url so that he can play his own audio
+        playAudio(custom_url,time?time:7);
+    }
